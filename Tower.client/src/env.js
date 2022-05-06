32485194b03072744b8ca061b5d6861f56780818
Maria Walker <mariawalker3232@gmail.com>
2022-05-06 11:28:40 -0600

    initial

---
+++ Tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-cqipusvi.us.auth0.com'
+export const clientId = 'https://MariaDev.com'
+export const audience = 'A5W8kG3Elb7hKFWQANsciWYCaMO5WwtL'
